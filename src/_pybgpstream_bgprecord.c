781a4b7586a153cae317994e31ceb59f8d100dff
Alistair King <alistair@caida.org>
2016-07-04 11:55:28 -0700

    Re-run clang-format
    This applies the reduced ContinuationIndentWidth

--- src/_pybgpstream_bgprecord.c
+++ src/_pybgpstream_bgprecord.c
@@ -21,11 +21,11 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "_pybgpstream_bgprecord.h"
+#include "_pybgpstream_bgpelem.h"
 #include "pyutils.h"
 #include <Python.h>
 #include <bgpstream.h>
-#include "_pybgpstream_bgpelem.h"
-#include "_pybgpstream_bgprecord.h"
 
 #define BGPRecordDocstring "BGPRecord object"
 
@@ -180,78 +180,77 @@ static PyObject *BGPRecord_get_next_elem(BGPRecordObject *self)
 
 static PyMethodDef BGPRecord_methods[] = {
 
-    {"get_next_elem", (PyCFunction)BGPRecord_get_next_elem, METH_NOARGS,
-     "Get next BGP Elem from the Record"},
+  {"get_next_elem", (PyCFunction)BGPRecord_get_next_elem, METH_NOARGS,
+   "Get next BGP Elem from the Record"},
 
-    {NULL} /* Sentinel */
+  {NULL} /* Sentinel */
 };
 
 static PyGetSetDef BGPRecord_getsetters[] = {
 
-    /* attributes.dump_project */
-    {"project", (getter)BGPRecord_get_project, NULL, "Dump Project", NULL},
+  /* attributes.dump_project */
+  {"project", (getter)BGPRecord_get_project, NULL, "Dump Project", NULL},
 
-    /* attributes.dump_collector */
-    {"collector", (getter)BGPRecord_get_collector, NULL, "Dump Collector",
-     NULL},
+  /* attributes.dump_collector */
+  {"collector", (getter)BGPRecord_get_collector, NULL, "Dump Collector", NULL},
 
-    /* attributes.dump_type */
-    {"type", (getter)BGPRecord_get_type, NULL, "Dump Type", NULL},
+  /* attributes.dump_type */
+  {"type", (getter)BGPRecord_get_type, NULL, "Dump Type", NULL},
 
-    /* attributes.dump_time */
-    {"dump_time", (getter)BGPRecord_get_dump_time, NULL, "Dump Time", NULL},
+  /* attributes.dump_time */
+  {"dump_time", (getter)BGPRecord_get_dump_time, NULL, "Dump Time", NULL},
 
-    /* attributes.record_time */
-    {"time", (getter)BGPRecord_get_record_time, NULL, "Record Time", NULL},
+  /* attributes.record_time */
+  {"time", (getter)BGPRecord_get_record_time, NULL, "Record Time", NULL},
 
-    /* status */
-    {"status", (getter)BGPRecord_get_status, NULL, "Status", NULL},
+  /* status */
+  {"status", (getter)BGPRecord_get_status, NULL, "Status", NULL},
 
-    /* attributes.dump_position */
-    {"dump_position", (getter)BGPRecord_get_dump_position, NULL,
-     "Dump Position", NULL},
+  /* attributes.dump_position */
+  {"dump_position", (getter)BGPRecord_get_dump_position, NULL, "Dump Position",
+   NULL},
 
-    {NULL} /* Sentinel */
+  {NULL} /* Sentinel */
 };
 
 static PyTypeObject BGPRecordType = {
-    PyVarObject_HEAD_INIT(NULL, 0) "_pybgpstream.BGPRecord", /* tp_name */
-    sizeof(BGPRecordObject),                                 /* tp_basicsize */
-    0,                                                       /* tp_itemsize */
-    (destructor)BGPRecord_dealloc,                           /* tp_dealloc */
-    0,                                                       /* tp_print */
-    0,                                                       /* tp_getattr */
-    0,                                                       /* tp_setattr */
-    0,                                                       /* tp_compare */
-    0,                                                       /* tp_repr */
-    0,                                                       /* tp_as_number */
-    0,                                        /* tp_as_sequence */
-    0,                                        /* tp_as_mapping */
-    0,                                        /* tp_hash */
-    0,                                        /* tp_call */
-    0,                                        /* tp_str */
-    0,                                        /* tp_getattro */
-    0,                                        /* tp_setattro */
-    0,                                        /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
-    BGPRecordDocstring,                       /* tp_doc */
-    0,                                        /* tp_traverse */
-    0,                                        /* tp_clear */
-    0,                                        /* tp_richcompare */
-    0,                                        /* tp_weaklistoffset */
-    0,                                        /* tp_iter */
-    0,                                        /* tp_iternext */
-    BGPRecord_methods,                        /* tp_methods */
-    0,                                        /* tp_members */
-    BGPRecord_getsetters,                     /* tp_getset */
-    0,                                        /* tp_base */
-    0,                                        /* tp_dict */
-    0,                                        /* tp_descr_get */
-    0,                                        /* tp_descr_set */
-    0,                                        /* tp_dictoffset */
-    (initproc)BGPRecord_init,                 /* tp_init */
-    0,                                        /* tp_alloc */
-    BGPRecord_new,                            /* tp_new */
+  PyVarObject_HEAD_INIT(NULL, 0) "_pybgpstream.BGPRecord", /* tp_name */
+  sizeof(BGPRecordObject),                                 /* tp_basicsize */
+  0,                                                       /* tp_itemsize */
+  (destructor)BGPRecord_dealloc,                           /* tp_dealloc */
+  0,                                                       /* tp_print */
+  0,                                                       /* tp_getattr */
+  0,                                                       /* tp_setattr */
+  0,                                                       /* tp_compare */
+  0,                                                       /* tp_repr */
+  0,                                                       /* tp_as_number */
+  0,                                                       /* tp_as_sequence */
+  0,                                                       /* tp_as_mapping */
+  0,                                                       /* tp_hash */
+  0,                                                       /* tp_call */
+  0,                                                       /* tp_str */
+  0,                                                       /* tp_getattro */
+  0,                                                       /* tp_setattro */
+  0,                                                       /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,                /* tp_flags */
+  BGPRecordDocstring,                                      /* tp_doc */
+  0,                                                       /* tp_traverse */
+  0,                                                       /* tp_clear */
+  0,                                                       /* tp_richcompare */
+  0,                        /* tp_weaklistoffset */
+  0,                        /* tp_iter */
+  0,                        /* tp_iternext */
+  BGPRecord_methods,        /* tp_methods */
+  0,                        /* tp_members */
+  BGPRecord_getsetters,     /* tp_getset */
+  0,                        /* tp_base */
+  0,                        /* tp_dict */
+  0,                        /* tp_descr_get */
+  0,                        /* tp_descr_set */
+  0,                        /* tp_dictoffset */
+  (initproc)BGPRecord_init, /* tp_init */
+  0,                        /* tp_alloc */
+  BGPRecord_new,            /* tp_new */
 };
 
 PyTypeObject *_pybgpstream_bgpstream_get_BGPRecordType()
