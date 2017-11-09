ef56a76b15dc05c396c76e8c33779db63b401f85
Alistair King <alistair@caida.org>
2017-11-09 11:53:58 -0800

    Add a prototype high-level python API
    This is a thin wrapper around `_pybgpstream` and should make it much more enjoyable to use BGPStream from within Python. See examples/pybgpstream-template.py for an example of how to use this API.

---
+++ examples/pybgpstream-template.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# This file is part of pybgpstream
+#
+# CAIDA, UC San Diego
+# bgpstream-info@caida.org
+#
+# Copyright (C) 2015 The Regents of the University of California.
+# Authors: Alistair King
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+This is a sample script that uses the high-level pybgpstream module.
+
+You can use this script as a starting point when creating new applications that use PyBGPStream
+"""
+
+import pybgpstream
+
+# create and configure the stream
+stream = pybgpstream.BGPStream(
+    from_time="2017-07-07 00:00:00", until_time="2017-07-07 00:10:00 UTC",
+    collectors=["route-views.sg", "route-views.eqix"],
+    record_type="updates",
+    filter="peer 11666 and prefix more 210.180.0.0/16"
+)
+
+# add any additional (or dynamic) filters
+# e.g. from peer AS 11666 regarding the more-specifics of 210.180.0.0/16:
+# stream.parse_filter_string("peer 11666 and prefix more 210.180.0.0/16")
+# or using the old filter interface:
+# stream.add_filter("peer-asn", "11666")
+# stream.add_filter("prefix-more", "210.180.0.0/16")
+
+# read elems
+for rec, elem in stream:
+    # do something with rec and elem
+    print "Received %s at time %d from collector %s" % (elem.type, rec.time, rec.collector)
+    if elem.type in ["A", "W"]:
+        print "  Peer ASN: %s, Pfx: %s" % (elem.peer_asn, elem.fields["prefix"])
+
+# alternatively, records and elems can be read in nested loops:
+for rec in stream.records():
+    # do something with rec (e.g., choose to continue based on timestamp)
+    print "Received %s record at time %d from collector %s" % (rec.type, rec.time, rec.collector)
+    for elem in rec:
+        # do something with rec and/or elem
+        print "  Elem Type: %s" % elem.type
